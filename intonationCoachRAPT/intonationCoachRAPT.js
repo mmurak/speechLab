ed81223502304bea0d39698a9745eb64260746d8
Masaaki Murakami <masaaki.murakami@gmail.com>
2026-09-20 10:42:01 +1200

    Update intonationCoachRAPT.js

--- intonationCoachRAPT/intonationCoachRAPT.js
+++ intonationCoachRAPT/intonationCoachRAPT.js
@@ -680,3 +680,4 @@ highFreqInput.addEventListener('change', reanalyzeWithCurrentRange);
 
 window.addEventListener('resize', layoutCanvas);
 layoutCanvas();
+window.addEventListener('load', layoutCanvas); // ← 追加: 全リソース読み込み完了後にも再計算する
